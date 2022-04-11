75e6b734b76afe08eb01a2c3a4226c79b1f33ecd
jronkemp <jronkemp@gmail.com>
2022-04-11 10:29:48 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# homework-nine
+## making a website responsive using JS and MVC framework
+
+<br/>
+<br/>
+
+For this homework assignment, we were tasked with creating a website that used the MVC framework with javascript. All content should be loaded dynamically through a single html landing page.
+
+Test the responsivness by using the navigation at the top of the screen.
+<br/>
+<br/>
+
+The link to the web4 page for this homework can be found below: <br/>
+https://in-info-web4.informatics.iupui.edu/~jakemps/homework-nine/
---
+++ model/model.js
@@ -0,0 +1,133 @@
+//data for web pages
+var galleryContent = `  <div class="gallery">
+<h1>Gallery</h1>
+<div class="gallery-images">
+    <div class="gallery-images-columnleft">
+        <div class="sixteenbynine">
+            <img src="images/Shot-2.jpg" alt="">
+        </div>
+        <div class="fourbyfive">
+            <img src="images/Shot-6.jpg" alt="">
+        </div>
+    </div>
+    <div class="gallery-images-columnmiddle">
+        <div class="fourbyfive">
+            <img src="images/Shot-1.jpg" alt="">
+        </div>
+        <div class="sixteenbynine">
+            <img src="images/Shot-45.jpg" alt="">
+        </div>
+    </div>
+    <div class="gallery-images-columnright">
+        <div class="sixteenbynine">
+            <img src="images/Shot-40.jpg" alt="">
+        </div>
+        <div class="fourbyfive">
+            <img src="images/Shot-17.jpg" alt="">
+        </div>
+    </div>
+</div>
+</div>`;
+
+var aboutContent = `<div class="about-page">
+<h1>Behind the Lens</h1>
+    <div class="about-photographer">
+        <div class="about-photographer-description">
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut placeat, minima corrupti sequi, veritatis repellendus praesentium, delectus repellat debitis voluptas doloribus quas asperiores ipsa vel doloremque sed ea impedit cumque ex adipisci voluptates nam? Eveniet culpa voluptate, iusto quam corporis alias fugit cupiditate nihil dolores unde vitae incidunt sint? Quas, reprehenderit beatae dolorem voluptates id, quos ad ratione sed consequatur nobis eius consectetur at error veritatis quibusdam, est omnis amet. Quibusdam, ducimus animi tempore maiores optio earum repudiandae ipsa explicabo aliquam ex incidunt ea distinctio tenetur blanditiis facilis hic consequuntur provident eos commodi itaque aspernatur voluptate delectus minus. Delectus deleniti eveniet eaque fugiat accusantium dolorum ab corrupti dignissimos magnam voluptatem tempore perferendis adipisci sit officiis nesciunt consequuntur, incidunt dolorem esse repellat suscipit doloribus? Consequuntur et dolor sit non excepturi, eaque ratione blanditiis magnam fuga quam molestiae eum veritatis aperiam sunt voluptatem voluptatum consectetur dolore laborum maiores aut, neque architecto quae ad. Magni vitae impedit perspiciatis, odio nihil illum. Debitis quae in quibusdam repudiandae vel! Facilis vitae illo iusto corporis magnam rerum esse dolores quibusdam perspiciatis, sequi odio voluptatum sed ullam consectetur sunt maxime. Pariatur ex facilis, unde distinctio aspernatur officiis! Reiciendis quibusdam ad ipsam necessitatibus temporibus, molestiae ipsa voluptas vitae?</p>
+        </div>
+        <div class="profile-pic">
+            <img src="images/IMG_0839.jpeg" alt="">
+            <h4>Jaron Kempson</h4>
+        </div>
+    </div>
+    <div class="about-photographer">
+        <div class="profile-pic">
+            <img src="images/kylin.jpg" alt="">
+            <h4>Kylin Kempson</h4>
+        </div>
+        <div class="about-photographer-description">
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut placeat, minima corrupti sequi, veritatis repellendus praesentium, delectus repellat debitis voluptas doloribus quas asperiores ipsa vel doloremque sed ea impedit cumque ex adipisci voluptates nam? Eveniet culpa voluptate, iusto quam corporis alias fugit cupiditate nihil dolores unde vitae incidunt sint? Quas, reprehenderit beatae dolorem voluptates id, quos ad ratione sed consequatur nobis eius consectetur at error veritatis quibusdam, est omnis amet. Quibusdam, ducimus animi tempore maiores optio earum repudiandae ipsa explicabo aliquam ex incidunt ea distinctio tenetur blanditiis facilis hic consequuntur provident eos commodi itaque aspernatur voluptate delectus minus. Delectus deleniti eveniet eaque fugiat accusantium dolorum ab corrupti dignissimos magnam voluptatem tempore perferendis adipisci sit officiis nesciunt consequuntur, incidunt dolorem esse repellat suscipit doloribus? Consequuntur et dolor sit non excepturi, eaque ratione blanditiis magnam fuga quam molestiae eum veritatis aperiam sunt voluptatem voluptatum consectetur dolore laborum maiores aut, neque architecto quae ad. Magni vitae impedit perspiciatis, odio nihil illum. Debitis quae in quibusdam repudiandae vel! Facilis vitae illo iusto corporis magnam rerum esse dolores quibusdam perspiciatis, sequi odio voluptatum sed ullam consectetur sunt maxime. Pariatur ex facilis, unde distinctio aspernatur officiis! Reiciendis quibusdam ad ipsam necessitatibus temporibus, molestiae ipsa voluptas vitae?</p>
+        </div>
+        
+    </div>
+</div>`;
+var blogContent = `<div class="blog">
+<h1>Blog</h1>
+
+<div class="collection">
+    <div class="date-released">
+      <p class="one">06</p>
+      <p class="two">JUN</p>
+    </div>
+    <div class="blog-info">
+
+      <div class="blog-image">
+          <img src="images/skyline.jpg" alt="">
+      </div>
+      <div class="blog-title">
+          <p>Vacation in Hawaii</p>
+      </div>
+      <div class="blog-description">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos mollitia architecto voluptate? Sed incidunt tempora ipsum quaerat. Ipsa porro error tempore dolores corporis? Sapiente, praesentium.</p>
+      </div>
+
+    </div>
+</div>
+
+<div class="collection">
+    <div class="date-released">
+      <p class="one">10</p>
+      <p class="two">JUN</p>
+    </div>
+    <div class="blog-info">
+
+      <div class="blog-image">
+          <img src="images/Shot-37.jpg" alt="">
+      </div>
+      <div class="blog-title">
+          <p>Vacation in Hawaii, Day 2</p>
+      </div>
+      <div class="blog-description">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos mollitia architecto voluptate? Sed incidunt tempora ipsum quaerat. Ipsa porro error tempore dolores corporis? Sapiente, praesentium.</p>
+      </div>
+
+    </div>
+</div>
+</div>`;
+var contactContent = `<div class="contact">
+<h1>Contact</h1>
+
+<div class="collection">
+    <div class="contact-image">
+        <img src="images/Shot-7.JPG" alt="">
+
+    </div>
+    <div class="contact-form">
+        <div class="top-boxes">
+            <input type="text" class="firstname" placeholder="First Name">
+            <input type="text" class="firstname" placeholder="Last Name">
+        </div>
+        <div class="email-box">
+            <input type="email" class="email" placeholder="Email">
+        </div>
+        <div class="subject-box">
+            <input type="text" class="subject" placeholder="Subject">
+        </div>
+        <div class="message-box">
+            <input type="text" class="message" placeholder="Message">
+        </div>
+        <div class="submit">
+            <p>Submit</p>
+        </div>
+        
+    </div>
+</div>
+
+</div>`;
+
+
+//sends page (name) information to the view (index.html)
+export function loadPageContent(pgContent) {
+  $("#app").html(eval(pgContent));
+  //   console.log(pgName);
+}
